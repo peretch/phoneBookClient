e79df20980b8307bd478dbd84c4c85c65fa33553
peretch <sebastian@peretch.com>
2020-10-12 21:40:09 -0300

    Added navbar

---
+++ components/Navbar.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import NavbarLink from './NavbarLink';
+
+const { getCredentials, clearCredentials } = require('../lib/auth');
+
+const Navbar = () => {
+  const [menuActive, setMenuActive] = useState(false);
+  const [userToken, setUserToken] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const { token } = getCredentials();
+    setUserToken(token);
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
+  return (
+    <nav className="w-full flex items-center justify-between flex-wrap px-6 py-4 bg-gray-900">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <a href="/">
+          Contacts app
+          <span className="font-semibold text-xl tracking-tight" />
+        </a>
+      </div>
+      <div className="block md:hidden">
+        <button
+          type="button"
+          className="flex items-center px-3 py-2 border rounded text-gray-100 border-teal-400 hover:text-white hover:border-white"
+          onClick={() => toggleMenu()}
+        >
+          <svg
+            className="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div className="w-full block flex-grow md:flex md:items-center md:w-auto sm:py-0 text-center sm:text-justify bg-gray-900 sm:bg-gray-900">
+        <div
+          className={`text-sm md:flex-grow ${menuActive === true &&
+            'hidden'} md:block`}
+        >
+          <NavbarLink href="https://github.com/peretch" text="GitHub" />
+        </div>
+        <div>
+          {userToken ? (
+            <button
+              type="button"
+              onClick={() => {
+                clearCredentials();
+                router.push('/login');
+              }}
+              tabIndex={0}
+              className="hidden sm:inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 md:mt-0"
+            >
+              Logout
+            </button>
+          ) : (
+            <Link href="/login">
+              <a className="hidden sm:inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 md:mt-0">
+                Login
+              </a>
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useContext, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Context } from '../store/Store';
 
+import Navbar from '../components/Navbar';
+
 const { getCredentials } = require('../lib/auth');
 const { getContacts } = require('../lib/api');
 
@@ -24,7 +26,6 @@ const Home = () => {
       token,
       page: state.contactsCurrentPage,
     });
-    console.log({ data });
     if (status === 200) {
       const { contacts, totalContacts, totalPages, page } = data;
       dispatch({ type: 'SET_CONTACTS', payload: contacts });
@@ -42,66 +43,69 @@ const Home = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.contactsCurrentPage]);
   return (
-    <div className="h-screen w-screen flex justify-center relative">
-      <div className="content-center h-auto w-screen">
-        <table className="w-full table-auto">
-          <thead className="bg-blue-800 text-white">
-            <tr>
-              <th className="p-2">Name</th>
-              <th className="p-2">Phone number</th>
-            </tr>
-          </thead>
-          <tbody>
-            {typeof state.contacts !== 'undefined' &&
-              state.contacts.map((contact, key) => (
-                <tr key={`contact-${contact._id}`}>
-                  <td
-                    className={`p-2 text-center ${key % 2 === 0 &&
-                      'bg-blue-100'}`}
-                  >
-                    {contact.name}
-                  </td>
-                  <td
-                    className={`p-2 text-center ${key % 2 === 0 &&
-                      'bg-blue-100'}`}
-                  >
-                    {contact.phone}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-        <div className="absolute bottom-0 mb-10 w-screen">
-          <div className="flex justify-between mt-4 mx-6">
-            <button
-              type="button"
-              tabIndex={0}
-              onKeyPress={() => {}}
-              className="text-blue-500 float-left"
-              onClick={() => {
-                dispatch({
-                  type: 'SET_CONTACTS_CURRENT_PAGE',
-                  payload: state.contactsCurrentPage - 1,
-                });
-              }}
-            >{`${'<'} Prev`}</button>
-            <button
-              type="button"
-              tabIndex={0}
-              onKeyPress={() => {}}
-              className="text-blue-500  float-right"
-              onClick={() => {
-                dispatch({
-                  type: 'SET_CONTACTS_CURRENT_PAGE',
-                  payload: state.contactsCurrentPage + 1,
-                });
-              }}
-            >{`Next ${'>'}`}</button>
-          </div>
-          <div className="flex justify-center mt-4">
-            <p className="text-lg">
-              Page {state.contactsCurrentPage} of {state.contactsPages}
-            </p>
+    <div className="h-screen w-screen relative">
+      <Navbar />
+      <div className="flex justify-center">
+        <div className="content-center h-auto w-screen">
+          <table className="w-full table-auto">
+            <thead className="bg-blue-800 text-white">
+              <tr>
+                <th className="p-2">Name</th>
+                <th className="p-2">Phone number</th>
+              </tr>
+            </thead>
+            <tbody>
+              {typeof state.contacts !== 'undefined' &&
+                state.contacts.map((contact, key) => (
+                  <tr key={`contact-${contact._id}`}>
+                    <td
+                      className={`p-2 text-center ${key % 2 === 0 &&
+                        'bg-blue-100'}`}
+                    >
+                      {contact.name}
+                    </td>
+                    <td
+                      className={`p-2 text-center ${key % 2 === 0 &&
+                        'bg-blue-100'}`}
+                    >
+                      {contact.phone}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <div className="absolute bottom-0 mb-4 w-screen">
+            <div className="flex justify-between mt-4 mx-6">
+              <button
+                type="button"
+                tabIndex={0}
+                onKeyPress={() => {}}
+                className="text-blue-500 float-left"
+                onClick={() => {
+                  dispatch({
+                    type: 'SET_CONTACTS_CURRENT_PAGE',
+                    payload: state.contactsCurrentPage - 1,
+                  });
+                }}
+              >{`${'<'} Prev`}</button>
+              <button
+                type="button"
+                tabIndex={0}
+                onKeyPress={() => {}}
+                className="text-blue-500  float-right"
+                onClick={() => {
+                  dispatch({
+                    type: 'SET_CONTACTS_CURRENT_PAGE',
+                    payload: state.contactsCurrentPage + 1,
+                  });
+                }}
+              >{`Next ${'>'}`}</button>
+            </div>
+            <div className="flex justify-center mt-4">
+              <p className="text-lg">
+                Page {state.contactsCurrentPage} of {state.contactsPages}
+              </p>
+            </div>
           </div>
         </div>
       </div>
